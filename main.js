71f6b49a898617ef60a0c7402dd5fd83188f6cfc
ChoosingAName <79235987+ChoosingAName@users.noreply.github.com>
2022-11-01 21:01:56 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ function startClassification(){
 function modelReady() {
     classifier.classify(gotResults);
 }
+var dog=0;
+var cat=0;
+var cow=0;
 function gotResults(error, results){
     if(error) {
         console.error(error);
@@ -13,21 +16,25 @@ function gotResults(error, results){
         random_number_r=Math.floor(Math.random()*255);
         random_number_g=Math.floor(Math.random()*255);
         random_number_b=Math.floor(Math.random()*255);
+        document.getElementById("result_count").innerHTML="detected dog-"+dog+" detected cat-"+cat+" detected cow-"+cow;
         document.getElementById("result_label").innerHTML='I can hear - '+results[0].label;
         document.getElementById("result_confidence").innerHTML='Accuracy - '+(results[0].confidence*100).toFixed[2]+'%';
         document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
         document.getElementById("result_confidence").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
-        img=document.getElementById('cat','dog','cow');
+        img=document.getElementById("animal_image");
         
 
         if (results[0].label=='cat') {
-            img.src='pexels-pixabay-104827.jpg';
+            img.src='cat.jpg';
+            cat=cat+1;
         }   else if(results[0].label=='dog'){
-            img.src='Screen Shot 2022-10-22 at 11.00.21 AM.png';
+            img.src='dog.png';
+            dog=dog+1;
         }   else if(results[0].label=='cow'){
-            img.src='monika-kubala-OpMfiq8nPIO-unsplash.jpg';
+            img.src='cow.jpg';
+            cow=cow+1;
         }   else{
-            img.src='Screen Shot 2022-10-22 at 11.04.40 AM.png';
+            img.src='ear.png';
         }
     }
 }
